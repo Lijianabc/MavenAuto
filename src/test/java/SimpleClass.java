e846eb37d348eab6e14154b1d5f3761a2dd4dc75
JianLi <jianyli@Jians-mac.local>
2016-11-29 16:55:30 +0800

    Initial commit

---
+++ src/test/java/SimpleClass.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by jianyli on 11/29/16.
+ */
+public class SimpleClass {
+
+    @Test
+    public void test() {
+        System.out.println("");
+    }
+}
