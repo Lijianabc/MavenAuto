f098a848ee1dd87b5672c25ae54c1424f663c570
JianLi <jianyli@Jians-mac.local>
2016-11-29 17:14:30 +0800

    Changing somthing

--- src/test/java/SimpleClass.java
+++ src/test/java/SimpleClass.java
@@ -1,12 +1,18 @@
 import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
-/**
- * Created by jianyli on 11/29/16.
- */
 public class SimpleClass {
 
     @Test
     public void test() {
+        System.out.println("");
+        assertThat("1 + 1 = 2" , 2,  equalTo(1 + 1));
+    }
+
+
+    @Test
+    public void base() {
         System.out.println("");
     }
 }
